66ed055fd596b87f3ac53f2765aebe78ca0203a4
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-03 03:05:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let timer=document.getElementById("timer");
+let date=new Date();
+setInterval(
+	function(){
+		date=new Date();
+		timer.innerHTML=date.toLocateString();
+	},1000
+);
